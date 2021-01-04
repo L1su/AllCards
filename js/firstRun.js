2001e286ab7860353c1c50651c4d50523f7d6f4d
Lisu <>
2021-01-04 18:29:29 +0100

    End: FirstRun

--- js/firstRun.js
+++ js/firstRun.js
@@ -5,3 +5,21 @@ for (let step = 1; step < (LengthElements +1); step++) {
     document.querySelectorAll(".FirstRun__slide")[(step - 1)].id = 'slide'+step+'';
     document.querySelector(".FirstRun__nav").innerHTML += '<a href="#slide'+step+'" class="FirstRun__element">'+step+'</a>';
 }
+
+
+let RulesAccep = localStorage.getItem("rules");
+let FirstRunBox = document.querySelector(".FirstRun");
+let RulesAccButton = document.querySelector(".slide__acceptrul");
+
+//LocalStorage zasady
+if(RulesAccep  == null){
+    FirstRunBox.classList.add("FirstRun--open");
+}
+
+
+RulesAccButton.addEventListener("click", function() {
+    localStorage.setItem("rules", true);
+    FirstRunBox.classList.remove("FirstRun--open");
+  });
+
+
